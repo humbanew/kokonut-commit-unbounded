103395ffb90e3c6a04f0cfa392fe4f1270220353
Humberto Bravo <humbandroid@gmail.com>
2025-10-07 00:25:52 -0300

    Evoluindo projeto

--- actions/base/src/main.ts
+++ actions/base/src/main.ts
@@ -1,4 +1,6 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { Octokit } from '@octokit/rest'
 import { KTerminal } from './KTerminal.js'
 import { KTerminalMessages } from './KTerminalMessages.js'
 import { KExceptions } from './KExceptions.js'
@@ -13,52 +15,11 @@ const kamblerInputs = Object.freeze({
     githubToken: String(core.getInput('github_token')),
     agentToken: String(core.getInput('agent_token')),
     agentModel: String(core.getInput('agent_model')),
-    agentApiBaseUrl: String(core.getInput('agent_api_base_url')),
-    agentApiVersion: String(core.getInput('agent_api_version')),
-    agentApiMaxRetries: Number(core.getInput('agent_api_max_retries')),
-    agentApiRetryDelay: Number(core.getInput('agent_api_retry_delay')),
-    agentReviewThreshold: Number(core.getInput('agent_review_threshold')),
-    agentMaxSuggestions: Number(core.getInput('agent_max_suggestions')),
-    agentTimeout: Number(core.getInput('agent_timeout')),
-    commitMessagePattern: String(core.getInput('commit_message_pattern')),
-    commitMessageHint: String(core.getInput('commit_message_hint')),
     failOnInvalidCommit: Boolean(core.getInput('fail_on_invalid_commit')),
-    reviewPullRequest: Boolean(core.getInput('review_pull_request')),
-    reviewCommits: Boolean(core.getInput('review_commits')),
-    reviewDrafts: Boolean(core.getInput('review_drafts')),
-    commentOnPr: Boolean(core.getInput('comment_on_pr')),
-    commentHeader: String(core.getInput('comment_header')),
-    commentFooter: String(core.getInput('comment_footer')),
-    automerge: Boolean(core.getInput('automerge')),
-    automergeMethod: String(core.getInput('automerge_method')),
-    automergeCommitTitle: String(core.getInput('automerge_commit_title')),
-    automergeCommitMessage: String(core.getInput('automerge_commit_message')),
-    prBranchProtection: Boolean(core.getInput('pr_branch_protection')),
-    prReviewApprovalCount: Number(core.getInput('pr_review_approval_count')),
-    prReviewDismissStale: Boolean(core.getInput('pr_review_dismiss_stale')),
-    prReviewRequireCodeOwner: Boolean(core.getInput('pr_review_require_code_owner')),
-    prLabels: String(core.getInput('pr_labels')),
-    prAssignees: String(core.getInput('pr_assignees')),
-    prReviewers: String(core.getInput('pr_reviewers')),
-    prTeamReviewers: String(core.getInput('pr_team_reviewers')),
-    dryRun: Boolean(core.getInput('dry_run')),
-    emailNotifications: Boolean(core.getInput('email_notifications')),
-    emailSubject: String(core.getInput('email_subject')),
-    emailBody: String(core.getInput('email_body')),
-    logLevel: String(core.getInput('log_level')),
-    logFile: String(core.getInput('log_file')),
-    timezone: String(core.getInput('timezone')),
-    slackWebhookUrl: String(core.getInput('slack_webhook_url')),
-    slackChannel: String(core.getInput('slack_channel')),
-    slackUsername: String(core.getInput('slack_username')),
-    slackIconEmoji: String(core.getInput('slack_icon_emoji')),
-    slackIconUrl: String(core.getInput('slack_icon_url')),
-    notionApiToken: String(core.getInput('notion_api_token')),
-    notionDatabaseId: String(core.getInput('notion_database_id')),
-    notionPageProperties: String(core.getInput('notion_page_properties')),
-    notionPageContent: String(core.getInput('notion_page_content'))
-});
-kamblerInputs
+    autoUpdateCommitMsgs: Boolean(core.getInput('auto_update_commit_msgs')),
+    onlyEnhanceCommits: Boolean(core.getInput('only_enhance_commits')),
+    maxCommitsScan: Number(core.getInput('max_commits_scan')),
+})
 
 /**
  * The main function for the action.
@@ -67,7 +28,93 @@ kamblerInputs
  */
 export async function run(): Promise<void> {
     try {
-        kamblerLibrarys.exceptions.prototype.validation('github_token', 'GitHub token is required.');
+        kamblerLibrarys.exceptions.prototype.validation(
+            'github_token',
+            'GitHub token is required.'
+        )
+
+        const githubManipulacao = new Octokit({
+            auth: kamblerInputs.githubToken
+        })
+
+        const listaCommits: string[] = [] 
+        ,commitModificadoArquivos: string[] = []
+        ,maxCommitsToProcess = kamblerInputs.maxCommitsScan;
+
+        // get commit list
+        const { data: commitList } = await githubManipulacao.rest.repos.listCommits({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo
+        })
+        if (commitList.length === 0) {
+            kamblerLibrarys.exceptions.prototype.validation(
+                'commit_list',
+                'No commits found in the repository.'
+            )
+        }
+
+        for (let i = 0; i < Math.min(commitList.length, maxCommitsToProcess); i++) {
+            const commit = commitList[i];
+            listaCommits.push(commit.commit.message);
+            // get modified files for each commit
+            const { data: commitData } = await githubManipulacao.rest.repos.getCommit({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                ref: commit.sha
+            });
+            const modifiedFiles = commitData.files?.map(file => file.filename) || [];
+            commitModificadoArquivos.push(`Commit: ${commit.commit.message}\nModified Files: ${modifiedFiles.join(', ')}`);
+        }
+        console.log('Commit Messages:\n', listaCommits.join('\n\n'));
+        console.log('Modified Files for each Commit:\n', commitModificadoArquivos.join('\n\n'));
+
+        // Validate the latest commit message
+        // This feature is currently disabled
+        // Uncomment the following lines to enable it
+        if (kamblerInputs.onlyEnhanceCommits != false) {
+            if (kamblerInputs.failOnInvalidCommit) {
+                const commitMessage = commitList[0].commit.message
+                console.log(commitMessage)
+                const isValidCommit = /^(feat|fix|refactor|perf|style|test|docs|build|ops|chore): (\w+)(\(.+\))?$/.test(commitMessage)
+                if (!isValidCommit) {
+                    kamblerLibrarys.exceptions.prototype.validation(
+                        'commit_message',
+                        'The latest commit message is not valid. It must include "feat", "fix", "refactor", "perf", "style", "test", "docs", "build", "ops" or "chore".'
+                    )
+                }
+            }
+        } else {
+            core.info(
+                kamblerLibrarys.terminalMessages.prototype.information(
+                    'Skipping commit message validation as only_enhance_commits is set to false.'
+                )
+            )
+        }
+
+        /**
+         * TODO: IDEA:
+         * Additional action logic can be added here
+         * Enhance commit messages with AI agent
+         * ================================================================
+         * Adicionar lógica para interagir com o agente de IA aqui
+         * ================================================================
+         */
+
+        // AUTO UPDATE COMMIT MESSAGES
+        if (kamblerInputs.autoUpdateCommitMsgs) {
+            core.info(
+                kamblerLibrarys.terminalMessages.prototype.information(
+                    'Auto update of commit messages is enabled, but this feature is not yet implemented.'
+                )
+            )
+        }
+
+        // If we reach this point, the action was successfully completed
+        core.info(
+            kamblerLibrarys.terminalMessages.prototype.success(
+                'Action completed successfully.'
+            )
+        )
     } catch (error) {
         // Fail the workflow run if an error occurs
         if (error instanceof Error) {
