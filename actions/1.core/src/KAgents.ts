c67792e59f063d52eeada8058783dc25c9881f08
Humberto Bravo <humbandroid@gmail.com>
2025-10-16 13:01:38 +0000

    Atualizando dicionário de modelos

--- actions/1.core/src/KAgents.ts
+++ actions/1.core/src/KAgents.ts
@@ -1,12 +1,24 @@
 interface Dictionary {
-    [key: string]: [string];
+    [key: string]: Array<string>;
 }
 
 export class KAgents {
     protected artificialIntelligenceServiceAPIKey: string = '';
-    protected artificialIntelligenceServiceManufacturer: string = 'openai';
-    protected artificialIntelligenceServiceModel: string = 'gpt-3.5-turbo';
-    protected availableAndAcceptedModels: Dictionary = {};
+    protected artificialIntelligenceServiceManufacturer: string = '';
+    protected artificialIntelligenceServiceModel: string = '';
+    protected availableAndAcceptedModels: Dictionary = {
+        "google": [
+            "gemini-2.5-pro",
+            "gemini-2.5-flash",
+            "gemini-2.5-flash-lite",
+            "gemini-2.0-flash",
+            "gemini-2.0-flash-lite",
+            "gemma-3n",
+            "gemma-3",
+            "gemma-2",
+            "gemma"
+        ]
+    };
 
     constructor(aisAPIKey: string, aisManufacturer: string, aisModel: string) {
         this.artificialIntelligenceServiceAPIKey = aisAPIKey;
